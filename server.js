fa50421e5724850342c847756ac81d94b6cff7cf
damansingh13 <101415046@georgebrown.ca>
2024-10-13 19:07:54 -0400

    first commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/v1", booksRoutes)
 
 app.route("/")
     .get((req, res) => {
-        res.send("<h1>MogoDB + Mongoose Example</h1>")
+        res.send("<h1>MongoDB + Mongoose Example</h1>")
     })
 
 app.listen(SERVER_PORT, () =>{
